bd873fab1a1db7a5a53e0f91bcbf519d7f5c8300
PurplesStrat <wyvern.exploits@gmail.com>
2026-02-01 10:58:37 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,247 @@
+export const DATA = {
+  news: [
+    { label: true, title: "Update 3", contents: "Moved Cydia to Jailbreaks." },
+    { label: true, title: "Update 2", contents: "Apple apps added." }
+  ],
+
+  apps: [
+    { label: true, title: "Note", contents: "Icons may differ." },
+      { name: 'KSign ✅', url: 'https://api.khoindvn.io.vn/03zH0o', img: 'img/pf/ksign.png', descriptions: 'China Academy of Railway Sciences' },
+  { name: 'KSign', url: 'https://api.khoindvn.eu.org/XSCvQT', img: 'img/pf/ksign.png', descriptions: 'Qingdao Rural Commercial Bank Co., Ltd ' }, 
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/g65UJy', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign Com V1', url: 'https://api.khoindvn.io.vn/ZsgCVX', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign Com V2', url: 'https://api.khoindvn.io.vn/VNNlkL', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/9VDZ4Y', img: 'img/pf/ksign.png', descriptions: 'Wasu Media & Network Co' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/h5YrG1', img: 'img/pf/ksign.png', descriptions: 'Luoyang Postal Administration' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/TsTFIu', img: 'img/pf/ksign.png', descriptions: 'Etisalat - Emirates Telecommunications Corporation' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/IUurbX', img: 'img/pf/ksign.png', descriptions: 'China National Heavy Duty Truck Group Co., Ltd' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/5PBonm', img: 'img/pf/ksign.png', descriptions: 'China Telecommunications Corporation' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/1H9iee', img: 'img/pf/ksign.png', descriptions: 'Tianjin University of Commerce' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/qYiNnB', img: 'img/pf/ksign.png', descriptions: 'VIETTEL GROUP' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/xwgru5', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V1', url: 'https://api.khoindvn.io.vn/KYD1nK', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V2', url: 'https://api.khoindvn.io.vn/EIpxft', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V3', url: 'https://api.khoindvn.io.vn/ZNciX8', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V4', url: 'https://api.khoindvn.io.vn/nBQF8h', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/5dujHz', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V1', url: 'https://api.khoindvn.io.vn/hZ8MlI', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V2', url: 'https://api.khoindvn.io.vn/Fdty3X', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V3', url: 'https://api.khoindvn.io.vn/AK7usE', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V4', url: 'https://api.khoindvn.io.vn/oIV0oN', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/6DNLtD', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V1', url: 'https://api.khoindvn.io.vn/j7XAya', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V2', url: 'https://api.khoindvn.io.vn/qNkRtG', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V3', url: 'https://api.khoindvn.io.vn/mTt9ip', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V4', url: 'https://api.khoindvn.io.vn/RX7IAf', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V5', url: 'https://api.khoindvn.io.vn/tGeKc0', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V6', url: 'https://api.khoindvn.io.vn/Deq7RD', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V7', url: 'https://api.khoindvn.io.vn/kcvpWe', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign ✅', url: 'https://api.khoindvn.io.vn/IDcW4P', img: 'img/pf/profile-pic.png', descriptions: 'China Academy of Railway Sciences' },
+  { name: 'ESign', url: 'https://api.khoindvn.eu.org/KXcveB', img: 'img/pf/profile-pic.png', descriptions: 'Qingdao Rural Commercial Bank Co., Ltd ' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/jAbzrt', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign Com V1', url: 'https://api.khoindvn.io.vn/RzkRFk', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign Com V2', url: 'https://api.khoindvn.io.vn/oC0s5H', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/ST2vo0', img: 'img/pf/profile-pic.png', descriptions: 'Luoyang Postal Administration' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/CbhJOR', img: 'img/pf/profile-pic.png', descriptions: 'Wasu Media & Network Co., Ltd' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Zxycmb', img: 'img/pf/profile-pic.png', descriptions: 'Etisalat - Emirates Telecommunications Corporation' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/11jtJ7', img: 'img/pf/profile-pic.png', descriptions: 'China National Heavy Duty Truck Group Co., Ltd.' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/JJW6Nj', img: 'img/pf/profile-pic.png', descriptions: 'China Telecommunications Corporation' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/6xAzUM', img: 'img/pf/profile-pic.png', descriptions: 'Tianjin University of Commerce' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Sco2A2', img: 'img/pf/profile-pic.png', descriptions: 'VIETTEL GROUP' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Q9sxJb', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V1', url: 'https://api.khoindvn.io.vn/Bhqw0c', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V2', url: 'https://api.khoindvn.io.vn/9UqTrd', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V3', url: 'https://api.khoindvn.io.vn/BcUSW0', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V4', url: 'https://api.khoindvn.io.vn/4jftNL', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/z4nsG3', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V1', url: 'https://api.khoindvn.io.vn/hMU3PR', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V2', url: 'https://api.khoindvn.io.vn/OBr8Zi', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V3', url: 'https://api.khoindvn.io.vn/gegf9d', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V4', url: 'https://api.khoindvn.io.vn/a1Gr0d', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/2nf4UO', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V1', url: 'https://api.khoindvn.io.vn/j6HQG8', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V2', url: 'https://api.khoindvn.io.vn/hvcTb2', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V3', url: 'https://api.khoindvn.io.vn/bJV2fn', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V4', url: 'https://api.khoindvn.io.vn/QQYC0K', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V5', url: 'https://api.khoindvn.io.vn/ylpuYQ', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V6', url: 'https://api.khoindvn.io.vn/vnAa35', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V7', url: 'https://api.khoindvn.io.vn/qYW0nK', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' } 
+  ],
+
+  certificates: [
+    {
+      name: "Khoindvn",
+      url: "https://khoindvn.io.vn/document/DNS/ESignCert.zip",
+      img: "img/pf/ksign.png",
+      descriptions: "Certificates for KSign are now built in."
+    }
+  ],
+
+  dns: [
+    {
+        name: 'Khoindvn DNS',
+        url: 'https://khoindvn.io.vn/document/DNS/signed_khoindvn.mobileconfig',
+        img: 'https://khoindvn.io.vn/img/pf/ksign.png',
+        descriptions: 'Custom DNS Settings'
+      },
+      {
+        name: 'KhoinMod [OLD] DNS',
+        url: 'https://senumy.com/install/e-sign/dns.mobileconfig',
+        img: 'https://github.com/purplesstrat.png',
+        descriptions: 'Custom DNS Settings'
+      },
+      {
+        name: 'Skibidi DNS',
+        url: 'https://skibiditech.co/mobileconfig.php',
+        img: 'https://skibiditech.co/icon.jpg',
+        descriptions: 'Custom DNS Settings'
+      }
+  ],
+
+  executors: [
+     {
+        name: 'Codex',
+        url: 'itms-services://?action=download-manifest&amp;url=https%3A%2F%2Fcodex-ios.com%2FInstall.plist',
+        img: 'https://codex-ios.com/apple-touch-icon.png',
+        descriptions: 'New Certificate',
+      },
+      {
+        name: 'Delta',
+        url: 'itms-services://?action=download-manifest&url=https%3A%2F%2Fdeltaios-executor.com%2FInstall21.plist',
+        img: 'https://deltaios-executor.com/Icon.jpg',
+        descriptions: 'New Certificate',
+      },
+      {
+        name: 'KRNL',
+        url: 'itms-services://?action=download-manifest&url=https://krnl-ios.com/Install2.plist',
+        img: 'https://krnl-ios.com/unnamed.png',
+        descriptions: 'New Certificate',
+      },
+      
+      {
+        name: 'ArceusX Neo',
+        url: 'https://egirl.help/arceusx',
+        img: 'https://easyfiles.cc/2025/10/412f0194-5699-4156-8482-f98ce8f66454/arceusx.png',
+        descriptions: 'Raw File',
+      },
+      {
+        name: 'AppleWare',
+        url: 'https://egirl.help/appleware',
+        img: 'https://easyfiles.cc/2025/10/f649d610-c80f-4c52-9fa8-52ed63325656/appleware.png',
+        descriptions: 'Raw File',
+      },
+      {
+        name: 'SkibX',
+        url: 'itms-services://?action=download-manifest&url=https://skibiditech.co/SkibX.plist',
+        img: 'https://skibx.com/icon.png',
+        descriptions: 'Custom SkibidiTech Executor',
+      }
+
+  ],
+
+  websites: [
+     {
+        name: 'Roblox Compiler',
+        url: 'https://purplesstrat.github.io/native-apps/compiler.html',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Roblox_Logo_2022.jpg',
+        descriptions: 'Roblox compiler for making scripts.',
+      },
+  {
+        name: 'ScriptBlox',
+        url: 'https://scriptblox.com',
+        img: 'https://scriptblox.com/favicon.ico',
+        descriptions: 'Script searcher for Roblox.',
+      },
+   {
+        name: 'NextDNS',
+        url: 'https://my.nextdns.io',
+        img: 'https://nextdns.io/static/og-image.jpg',
+        descriptions: 'iOS DNS Maker, which can be used to make anti revoke DNS!',
+      },
+  ],
+
+  ipas: [
+     {
+        name: 'Discord (Revenge)',
+        url: 'https://download1648.mediafire.com/smrbdzokrqugf6sg2I70Y9ggCCXg_57RwrgWr0vdAoLPTSwrldD7i9zANAvLwG_KBhkNzeFVdNcbxQ08wd3zLk1ygNeeisFnsDjEMDOnNab-JQD-CLor8IAxrVkujdJjPXpUuGgzFPSH5ec4IttxSF19VOik-q1tt1gLB8mrHZOmfQ/cgs8oyq95416d2a/Revenge.ipa',
+        img: 'https://github.com/revenge-mod.png',
+        descriptions: 'Revenge Mod for Discord',
+      },
+  {
+        name: 'Discord (Bunny)',
+        url: 'https://download1648.mediafire.com/9i2xn9up8ehgnsP_rkAO7iro-336ANH2AjYPL_UgATKfTPrcppnFoN-ZkHaWU74wKtd5zl2L0h614ByxI1a91LCWYgqStN3aVUxCVPSzo7PiY7MNhqRiPSGKYR-OwfMNJanULwW6RxOw4kQ8Dj0PIgi9wptclTzXyFLjAI66hCJpbg/2t8bfrlykxk2vsy/Bunny.ipa',
+        img: 'https://github.com/bunny-mod.png',
+        descriptions: 'Bunny Mod for Discord',
+      },
+   {
+        name: 'Discord (Enmity)',
+        url: 'https://github.com/enmity-mod/tweak/releases/latest/download/Enmity.ipa',
+        img: 'https://github.com/enmity-mod.png',
+        descriptions: 'Enmity Mod for Discord',
+      },
+  ],
+
+  jailbreaks: [
+   
+      { label: true, title: "Jailbreaks", contents: "This section uses Jailbreaks.app API." },
+      { label: true, title: "Note", contents: "Cydia doesn't work by the way." },
+      {
+        name: 'Cydia+',
+        url: 'itms-services://?action=download-manifest&url=https://easyfiles.cc/2025/7/6127141a-6264-43ca-95b8-08653ad13dde/Cydia++.plist',
+        img: 'https://www.mobipicker.com/wp-content/uploads/2016/07/Cydia.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'
+      },
+           {
+        name: 'Chimera',
+        url: 'https://api.jailbreaks.app/install/Chimera',
+        img: 'https://jailbreaks.app/cdn/images/Chimera.png',
+        descriptions: 'Chimera Jailbreak.',
+      },
+      {
+        name: 'Unc0ver',
+        url: 'https://api.jailbreaks.app/install/Unc0ver',
+        img: 'https://unc0ver.dev/Icon.png',
+        descriptions: 'Unc0ver Jailbreak.',
+      },
+  ],
+
+  apple: [
+     
+                  {
+        name: 'Field Test',
+        url: 'https://www.icloud.com/shortcuts/25136abc5e1f44648a529012f431c425',
+        img: 'https://purplesstrat.github.io/app/IMG_0345.jpeg',
+        descriptions: 'Field Test App',
+      },
+           {
+        name: 'Face ID',
+        url: 'https://download1479.mediafire.com/31rki5szsk9gh78ODYhr4gV2qYFK2DfWC7ObfyV238j_HOvfyDkgFnX3GmazAufR76gjKyUDW5NSmci8EuQZ6l-BhQh_MclHLMJJcgL3TCekA1iwyROL8cIcYYDB40mtonBrYvEkyS0ZS04hdU-HUMiD2El2spMFW-W6cqltFvfEVA/yixwy3ti0fwj7hu/Face+ID_2.0.0_1754322687.ipa',
+        img: 'https://www.theiphonewiki.com/w/images/thumb/7/75/Face_ID_Icon.png/177px-Face_ID_Icon.png',
+        descriptions: 'Face ID App',
+      },
+  {
+        name: 'DemoLoop',
+        url: 'https://download1509.mediafire.com/wk0qnre4jkggUkg2_-G0P83mkNgH4eHAcN2gyrrYM0FkXKpeEKCD_eUIkXg9Vi6OsGNySGl2XqkUDPISv05F-_DiW7IihFd5zmJFaYd2TEYJLLF8_1s8hEdI3LqS2aJLsBE46vyMhrWn-ZTk3OVlcAMGmqIyeHSSHK_TlD8PADoOPw/itcgesnvz7yrv3x/DemoLoop_7.4_1754322656.ipa',
+        img: 'https://i.imgur.com/MMMcgNV.png',
+        descriptions: 'DemoLoop app.',
+      },
+   {
+        name: 'DemoLoop 2',
+        url: 'https://download1589.mediafire.com/etgac7b5z5tg_6vGu2YTkljTmhDYk72lq9uNYI79yUugFAxFCTGCV0wKuCN8VSntCmxefysxzNGMLZ-NpiTlwYy271HZ0Xv873rqju50fnrR7Narl95DqHtI0f9DX1UpotocamWAnXtlt3kY0xqnkb8PA8MOeQDRKvOd_h4na9VP-Q/kyt59mgt3a937nu/DemoLoop_1.3.0_1754322491Official.ipa',
+        img: 'https://i.imgur.com/MMMcgNV.png',
+        descriptions: 'DemoLoop second version if first one does not work.',
+      },
+       {
+        name: 'Meet iPhone',
+        url: 'https://download1339.mediafire.com/v71ouavhg4lgqvUkJLCntZV2RR5szAplUG0PYaHBiQEUrKhPJzUwqivNS65nsdIFAm9u5cCbV-jKyZjl01WgAJUiGTGST-QrbZg1ByNiyvfqfzpTyPah7CeNVdHNJiX3CcM0K3b2HTSZD6rBYIShEirVncS-uic_1EGE8Kck17ClXA/yda1etj72jup427/Meet+iPhone_4.4.0.8_1754322641.ipa',
+        img: 'https://i.imgur.com/Ge1SkCy.png',
+        descriptions: 'Meet iPhone app.',
+      },
+       {
+        name: 'Touch ID',
+        url: 'https://download1979.mediafire.com/4gbnvnfv38zgFSRpdRdOqrMxOKN_hEsXiXDHsWR6z_HFoKVWhx2U42o4gyiS4KKUaFBBoQFhmffCUA_9cGoRFGXBmpqXBHeZA2jmB4hqcBfxeSa6pCJwYeO79g2RC8mnAtW2Sq3KuMQdPV90E4PLeVGOowXLN501jdJb3QXwH1nFOQ/0y1ghp3mnhc617u/Touch+ID_3.2.0_1754322645.ipa',
+        img: 'https://i.imgur.com/gSI1mfY.png',
+        descriptions: 'Touch ID App',
+      },
+  ]
+};
